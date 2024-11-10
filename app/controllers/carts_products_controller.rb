883df128a3929728d52e2fc13af086f7f965e0cc
Hamza Shariq <hamzashariq123@gmail.com>
2024-11-10 23:09:23 +0500

    Added delete from cart functionality

--- app/controllers/carts_products_controller.rb
+++ app/controllers/carts_products_controller.rb
@@ -1,12 +1,9 @@
 class CartsProductsController < ApplicationController
-  def delete
-  end
-
   def increment
     @cart_product = current_user.cart.carts_products.find(params[:id])
     @cart_product.update!(quantity: @cart_product.quantity + 1)
 
-    render turbo_stream: turbo_stream.replace("cart-item-#{@cart_product.id}", partial: "carts/cart_product", locals: { cart_product: @cart_product })
+    render turbo_stream: turbo_stream.replace("cart-product-#{@cart_product.id}", partial: "carts/cart_product", locals: { cart_product: @cart_product })
   end
 
   def decrement
@@ -14,8 +11,17 @@ class CartsProductsController < ApplicationController
 
     if @cart_product.quantity > 1
       @cart_product.update!(quantity: @cart_product.quantity - 1)
+      render turbo_stream: turbo_stream.replace("cart-product-#{@cart_product.id}", partial: "carts/cart_product", locals: { cart_product: @cart_product })
+    else
+      @cart_product.destroy
+      render turbo_stream: turbo_stream.remove("cart-product-#{@cart_product.id}")
     end
+  end
 
-    render turbo_stream: turbo_stream.replace("cart-item-#{@cart_product.id}", partial: "carts/cart_product", locals: { cart_product: @cart_product })
+  def destroy
+    @cart_product = current_user.cart.carts_products.find(params[:id])
+
+    @cart_product.destroy
+    render turbo_stream: turbo_stream.remove("cart-product-#{@cart_product.id}")
   end
 end
